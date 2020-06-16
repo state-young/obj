2d5cc4b6fde5b185a5968ecc4f035132f65800e8
deta <549704828@qq.com>
2020-06-16 22:41:48 +0800

    post提交完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,44 +1,22 @@
-$(function () {
-  // layui是全局对象，通过它可以得到form对象
-  var form = layui.form
-  // 基于LayUI自定义表单验证规则
-  form.verify({
-    // 必须是6-8位字符,不包括空格
-    uname: [/^[\S]{6,8}$/, '用户名必须是6-8位字符'],
-    // 密码必须是6位数字
-    pwd: function (value, item) {
-      // 形参value标书对应输入域的值
-      // item表示DOM元素
-      // 验证6位数字
-      var reg = /^\d{6}$/
-      // 如果规则不匹配就返回提示
-      if (!reg.test(value)) {
-        return '密码必须是6位数字'
-      }
-    }
-  })
-
-  // 控制表单的提交
-  $('.layui-form').submit(function (e) {
-    // 阻止表单默认提交行为
+// 控制表单
+$(function(){
+ 
+  $('.layui-form').submit(function(e){
     e.preventDefault()
-    // 获取表单输入域的用户名和密码
-    // username=asdf&password=asdffffff
+    // 获取表单的登录名和密码
     var formData = $(this).serialize()
-    // 提交表单之前需要做表单验证，如果自己实现有点繁琐，所以可以借助LayUI实现
-    
-    // 调用后台接口验证是否正确
+    // 调[]用后台接口
     $.ajax({
-      type: 'post',
-      url: 'http://ajax.frontend.itheima.net/api/login',
-      data: formData,
-      success: function (res) {
-        // 登录成功后，跳转到主页面
+      type : 'post',
+      url : 'http://ajax.frontend.itheima.net/api/login',
+      data : formData,
+      success : function(res){
         if (res.status === 0) {
-          location.href = './index.html'
+          location .href = './index.html'
         }
+        
       }
     })
+    
   })
-  
 })
