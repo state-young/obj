0f2bc1aec13a82f7c059e014d92a356569df75a0
deta <549704828@qq.com>
2020-06-16 23:21:31 +0800

    js完成验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,18 @@
 // 控制表单
 $(function(){
- 
+  // 表单验证
+  var form = layui.form;
+  form.verify({
+    username : [ /^[\S]{6,8}$/,'请输入6-8位数字'],
+    pwd : function(value,item){
+      var teg = /^\d{6}$/
+      if (!teg.test(value)) {
+        return '密码必须是6位数字'
+      }
+    }
+   
+  })
+
   $('.layui-form').submit(function(e){
     e.preventDefault()
     // 获取表单的登录名和密码
