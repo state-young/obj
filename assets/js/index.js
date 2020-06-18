0c84e3755d601dd8af7a146ca4fe2c1ab648ada1
deta <549704828@qq.com>
2020-06-18 22:26:38 +0800

    头像更换

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,33 @@ $(function(){
     if (!mytoken) {
         location.href = './login.html'
     }
+   function loadUserInfo(){
+       $.ajax({
+           type: 'get',
+           url: 'http://ajax.frontend.itheima.net/my/userinfo',
+           headers: {
+            Authorization: localStorage.getItem('mytoken')
+           },
+            success : function(res){
+                if (res.status === 0) {
+                    // 获取用户信息
+                    var info = res.data
+                    info.user_pic = 'http://t.cn/RCzsdCq"'
+                    $('#welcome-username').html(info.username)
+                    $('#nav-username').html(info.username)
+                    // 头像数据
+                    if (info.user_pic) {
+                        //存在就显示图片
+                        $('#welcome-username').parent().siblings('div').remove()
+                        $('#welcome-username').parent().prepend('<img src="'+info.user_pic+'" alt="" />')
+                        $('#nav-username').siblings().remove()
+                        $('#nav-username').prepend('<img src="'+info.user_pic+'" alt="" />')
+
+
+                    }
+                }
+            }
+       })
+   }
+   loadUserInfo()
 })
