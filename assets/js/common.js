e8875ad2a98f0cb504c01dab82b8fbabe950e8d0
deta <549704828@qq.com>
2020-06-19 21:38:11 +0800

    通用设置完成new

--- assets/js/common.js
+++ assets/js/common.js
@@ -11,24 +11,28 @@
 
 var baseURL = 'http://ajax.frontend.itheima.net/'
 $.ajaxPrefilter(function(option){
+    // 请求开始之前
+    option.beforeSend = function(){
+        window.NProgress &&  window.NProgress.start()
+       
+    }
     // 通用地址的设置
     option.url = baseURL + option.url
     // 设置接口的请求头
     if (option.url.lastIndexOf('/my/') !== -1) {
         option.headers = {
             Authorization: localStorage.getItem('mytoken')
-        }
+        }}
         option.complete = function(res){
+            window.NProgress &&  window.NProgress.done()
             if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
                 // 清除无效的token
                 localStorage.removeItem('mytoken')
                   // 如果身份验证失败了，就跳转到登录页面
                   location.href = './login.html'
+                 
             
         }
     }
-   
-    
-}
 
 })
