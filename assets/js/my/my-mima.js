8e5b1318d9f6b925fa36b41b2b5e0b0dbaefc7e5
deta <549704828@qq.com>
2020-06-20 00:50:39 +0800

    密码完成提交和验证

--- assets/js/my/my-mima.js
+++ assets/js/my/my-mima.js
@@ -1,4 +1,26 @@
 $(function(){
+    // 表单验证
+    var form =layui.form
+    form.verify({
+        diff:function(value){
+            // 获取原来的密码
+            var old = $('#form input[name = oldPwd]').val()
+            if(oldPwd === value){
+                return '输入的密码不能和原来的密码相同'
+            }
+        },
+        same:function(){
+            // 2.确认密码必须一致
+             // 获取新的的密码
+            var newold = $('#form input[name = newPwd]').val()
+            if (newpwd !== value) {
+                return '两次输入的密码不相同'
+            }else{
+
+            }
+        }
+    })
+
     $('#form').submit(function(e){
         e.preventDefault()
         var fd = $(this).serialize()
@@ -8,6 +30,8 @@ $(function(){
             data : fd,
             success :function(res){
                 if (res.status === 0) {
+                    layer.msg(res.message)
+                }else{
                     layer.msg(res.message)
                 }
             }
